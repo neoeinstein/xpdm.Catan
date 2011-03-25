cab7433639153744357b584ef86c1d9666a3bd0e
Marcus Griep <marcus@griep.us>
2011-03-25 11:47:55 -0400

    Begin implementing descriptions for skins

---
+++ xpdm.Catan/Skins/SkinDescription.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows;
+
+namespace xpdm.Catan.Skins
+{
+    public class SkinDescription : FrameworkElement
+    {
+        public string DisplayName
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public Visual Preview
+        {
+            get;
+            set;
+        }
+
+        public string CopyrightNotice
+        {
+            get;
+            set;
+        }
+
+        public string License
+        {
+            get;
+            set;
+        }
+
+        public string LicenseUri
+        {
+            get;
+            set;
+        }
+    }
+}
